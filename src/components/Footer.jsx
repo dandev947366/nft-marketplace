0d77e9021eeeed03e1986b116ae5d42d03043f2f
Dan <dan.dev947366@gmail.com>
2024-12-26 19:41:43 +0200

    Fix Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="">
-      <div className="pt-8 mt-20 mx-auto bg-white border-t-2 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 shadow-[0px_-50px_100px_-49px_rgba(34,_197,_94,_0.5)]">
+      <div className="w-full pt-8 mt-20 mx-auto bg-white border-t-2 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 shadow-[0px_-50px_100px_-49px_rgba(34,_197,_94,_0.5)]">
         <div className="grid gap-12 row-gap-6 px-4 mb-8 md:px-8 sm:grid-cols-2 lg:grid-cols-4">
           <div className="sm:col-span-2">
             <Link
@@ -62,7 +62,7 @@ const Footer = () => {
 
         <div className="flex flex-col-reverse justify-between gap-2 px-4 pt-4 pb-8 text-sm border-t md:px-8 dark:border-gray-600 lg:flex-row">
           <p className="">© 2024 NFTMarketplace. All Rights Reserved.</p>
-          Made with <span className="text-red-500">&#10084;</span> by Dan Le
+          <p>Made with <span className="mr-1 ml-1 text-red-500">&#10084;</span> by Dan Le</p>
           <ul className="flex flex-col gap-2 mb-4 text-sm sm:flex-row md:mb-0">
             <li className="md:border-r-2 dark:border-gray-600 md:pr-2">
               <Link to="https://danle.dev/">FAQs</Link>
--- src/views/NFT.jsx
+++ src/views/NFT.jsx
@@ -2,60 +2,12 @@ import { Gavel, Wallet, UserRoundPen, Trophy } from "lucide-react";
 import { setGlobalState, truncate, useGlobalState } from "../store";
 import picture4 from "../assets/images/picture4.png";
 import Footer from "../components/Footer";
+import CountDown from "../components/CountDown";
 
 const Details = ({ auction, account }) => {
   return (
     <>
-      <div className="">
-        <p className="text-[#BFC4CA] py-1 font-semibold text-2xl mb-11">
-          Name:{auction.name}
-        </p>
-        <p className="text-[#BFC4CA] py-1 font-semibold text-3xl mb-11">
-
-          Owner:{auction?.owner == account
-            ? "You"
-            : truncate(account?.owner, 4, 4, 11)}
-        </p>
-        <p className="text-[#BFC4CA] py-1 font-semibold text-3xl mb-11">
-          Description: {auction.description}
-        </p>
-      </div>
-    </>
-  );
-};
-
-const Bidders = ({ bidders, auction }) => {
-  return (
-    <>
-      <div className="">
-        {/* <Trophy
-          className=""
-          size={18}
-          string={"wallet"}
-        /> */}
-        <span>{truncate("", 4, 4, 11)}</span>
-        {/* <span>2 ETH</span> */}
-      </div>
-      {/* <button>Claim Prize</button> */}
-    </>
-  );
-};
-const NFT = () => {
-  const [openBox] = useGlobalState("openBox");
-  const [auction] = useGlobalState("auction");
-  const [bidders] = useGlobalState("bidders");
-  const [connectedAccount] = useGlobalState("connectedAccount");
-
-  const onChange = () => {
-    setGlobalState("boxModal", "scale-100");
-    setGlobalState("openBox", true);
-  };
-
-  return (
-    <div>
-      <div className="w-4/5 flex flex-col mt-11 mx-auto bg-white border border-gray-300 rounded-md shadow-md -z-10 dark:bg-gray-800 dark:border-gray-700 shadow-[0px_0px_100px_-24px_rgba(147,_51,_234,_0.5)]">
-        <div className={openBox ? "opacity-50" : "opacity-100"}>
-          <div className="relative my-4 mx-4 md:flex-row w-full justify-between items-center mx-auto">
+      <div className="relative my-4 mx-4 md:flex-row w-full justify-between items-center mx-auto">
             <div className="mx-4 flex text-white py-1 font-semibold text-5xl ml-auto">
               <img
                 src={picture4}
@@ -87,49 +39,93 @@ const NFT = () => {
               </div> */}
             </div>
           </div>
+    </>
+  );
+};
+
+const Bidders = ({ bidders, auction }) => {
+  return (
+    <>
+      <div
+            title="total views"
+            className="flex items-center gap-1 p-3 text-lg text-gray-700 rounded bg-gray-300/50 dark:bg-gray-700/50 dark:text-gray-300"
+          >
+            <Wallet className="mr-2" /> x09082jsdadaug2.....
+            <p className="ml-auto mr-10">0.04 ETH</p>
+            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
+          </div>
+          <div
+            title="total views"
+            className="flex items-center gap-1 p-3 text-lg text-slate-300"
+          >
+            <Wallet className="mr-2" /> x09082jsdadaug2.....
+            <p className="ml-auto mr-10">0.04 ETH</p>
+            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
+          </div>
+          <div
+            title="total views"
+            className="flex items-center gap-1 p-3 text-lg text-gray-700 rounded bg-gray-300/50 dark:bg-gray-700/50 dark:text-gray-300"
+          >
+            <Wallet className="mr-2" /> x09082jsdadaug2.....
+            <p className="ml-auto mr-10">0.04 ETH</p>
+            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
+          </div>
+          <div
+            title="total views"
+            className="flex items-center gap-1 p-3 text-lg text-slate-300"
+          >
+            <Wallet className="mr-2" /> x09082jsdadaug2.....
+            <p className="ml-auto mr-10">0.04 ETH</p>
+            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
+          </div>
+    </>
+  );
+};
+
+const CountDownPrice = ({auction}) => {
+  return(
+    <>
+      <span className="text-indigo-600 ml-2">{auction?.duration > Date.now() ? (<CountDown timestamp={auction.duration}/>) : '00:00:00'}</span>
+
+    </>
+  )
+}
+
+
+const NFT = () => {
+  const [openBox] = useGlobalState("openBox");
+  const [auction] = useGlobalState("auction");
+  const [bidders] = useGlobalState("bidders");
+  const [connectedAccount] = useGlobalState("connectedAccount");
+
+  const onChange = () => {
+    setGlobalState("boxModal", "scale-100");
+    setGlobalState("openBox", true);
+  };
+
+  return (
+    <div>
+      <div className="w-4/5 flex flex-col mt-11 mx-auto bg-white border border-gray-300 rounded-md shadow-md -z-10 dark:bg-gray-800 dark:border-gray-700 shadow-[0px_0px_100px_-24px_rgba(147,_51,_234,_0.5)]">
+        <div className={openBox ? "opacity-50" : "opacity-100"}>
+          <Details />
         </div>
       </div>
-      <div className="w-4/5 flex flex-col mt-11 mx-auto bg-white border border-gray-300 rounded-md shadow-md hover:shadow-indigo-500 -z-10 dark:bg-gray-800 dark:border-gray-700 h-[calc(130vh_-_40.5rem)] overflow-y-auto">
+      <div className="w-4/5 flex flex-col mt-11 mx-auto bg-white border border-gray-300 rounded-md shadow-md hover:shadow-indigo-500 -z-10 dark:bg-gray-800 dark:border-gray-700 h-[calc(140vh_-_40.5rem)] overflow-y-auto">
 
         <div className={openBox ? "opacity-50" : "opacity-100"}>
           <div className="relative my-4 mx-4 md:flex-row w-full justify-between items-center mx-auto">
             <p className="my-4 mx-4 text-white py-1 font-semibold text-2xl">
               Bidding History
             </p>
+            <p className="my-4 mx-4 flex text-white py-1 font-semibold text-2xl dark:bg-gray-800 dark:border-gray-700 text-indigo-600">
+              Current Price: 2 ETH
+            </p>
           </div>
-          <div className="my-7 mx-4 flex items-center gap-2 mb-5 pb-5 dark:text-gray-300 lg:mb-0"></div>
-          <div
-            title="total views"
-            className="flex items-center gap-1 p-3 text-lg text-gray-700 rounded bg-gray-300/50 dark:bg-gray-700/50 dark:text-gray-300"
-          >
-            <Wallet className="mr-2" /> x09082jsdadaug2.....
-            <p className="ml-auto mr-10">0.04 ETH</p>
-            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
-          </div>
-          <div
-            title="total views"
-            className="flex items-center gap-1 p-3 text-lg text-slate-300"
-          >
-            <Wallet className="mr-2" /> x09082jsdadaug2.....
-            <p className="ml-auto mr-10">0.04 ETH</p>
-            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
-          </div>
-          <div
-            title="total views"
-            className="flex items-center gap-1 p-3 text-lg text-gray-700 rounded bg-gray-300/50 dark:bg-gray-700/50 dark:text-gray-300"
-          >
-            <Wallet className="mr-2" /> x09082jsdadaug2.....
-            <p className="ml-auto mr-10">0.04 ETH</p>
-            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
-          </div>
-          <div
-            title="total views"
-            className="flex items-center gap-1 p-3 text-lg text-slate-300"
-          >
-            <Wallet className="mr-2" /> x09082jsdadaug2.....
-            <p className="ml-auto mr-10">0.04 ETH</p>
-            <button className="flex bg-indigo-600 justify-center items-center w-[100px] text-white text-md p-1 rounded-md drop-shadow-xl border border-transparent hover:text-white">Claim</button>
-          </div>
+          <Bidders />
+
+          <span className="my-4 mx-4 flex text-white py-1 font-semibold text-2xl">
+              Count Down:<CountDownPrice auction={auction} />
+          </span>
         </div>
       </div>
       <Footer />
