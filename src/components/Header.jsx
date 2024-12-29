90a9cebb2e48e2e3e4e70f1d6462f9b101f1a142
Dan <dan.dev947366@gmail.com>
2024-12-30 01:21:48 +0200

    Add metamask to index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSDK } from "@metamask/sdk-react";
-import axios from "axios";
 
 const Header = () => {
   const [account, setAccount] = useState(null);
--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
   ReactDOM.render(
     <BrowserRouter>
+      <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "NFT Marketplace",
+          url: window.location.href,
+        },
+        infuraAPIKey: process.env.INFURA_API_KEY,
+        // Other options
+      }}
+    >
       <App />
+    </MetaMaskProvider>
     </BrowserRouter>,
     document.getElementById('root'),
   )
